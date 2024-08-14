eb88daba905b3bb0142aacf568b8661adc1c02d2
jcglas2001 <jcglass2001@gmail.com>
2024-08-14 09:01:36 -0400

    initial commit

---
+++ src/main/java/com/jcg/cicdpipeline/CicdpipelineApplication.java
@@ -0,0 +1,12 @@
+package com.jcg.cicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ContextIdApplicationContextInitializer;
+
+@SpringBootApplication
+public class CicdpipelineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CicdpipelineApplication.class, args);
+    }
+}
